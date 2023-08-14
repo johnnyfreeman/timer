fdff0e02d1a3b305fee24950c62490c4771c23a6
John Freeman <prsjohnny@gmail.com>
2023-08-13 22:04:35 -0400

    duration now accepts 1s, 1m, and 1h format

--- src/main.rs
+++ src/main.rs
@@ -7,29 +7,42 @@ use std::time::Duration;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[arg(short, long, default_value_t = 1)]
-    duration: u64,
+    #[arg(short, long)]
+    duration: String,
     #[arg(short, long)]
     name: Option<String>,
 }
 
 fn main() {
     let args = Args::parse();
-
-    let pb = ProgressBar::new(args.duration);
+    let seconds = parse_duration_from_str(&args.duration);
+    let pb = ProgressBar::new(seconds);
     let local: DateTime<Local> = Local::now();
 
     if let Some(name) = args.name {
         println!(
-            "{}: {} - {}s",
+            "{}: {} - {}",
             local.format("%I:%M %p"),
             name,
-            args.duration
+            &args.duration
         );
     }
 
-    for _ in 0..args.duration {
+    for _ in 0..seconds {
         thread::sleep(Duration::from_secs(1));
         pb.inc(1);
+    }
+}
+
+fn parse_duration_from_str(input: &str) -> u64 {
+    let last_char = input.chars().last().unwrap_or_else(|| 's');
+    let numeric_part: &str = &input[..input.len() - 1];
+    let value: u64 = numeric_part.parse().ok().unwrap();
+
+    match last_char {
+        's' => value,
+        'm' => value * 60,
+        'h' => value * 60 * 60,
+        _ => 0,
     }
 }
